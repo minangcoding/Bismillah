ba21fe47f8a73423315673cfad29c4a73fd7ab9a
Refido Arjunal Akmal <refidoarjunalakmal@gmail.com>
2026-03-08 15:32:49 +0700

    feat: Upgrade website menjadi PWA lengkap dengan ikon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['OIP.png'], // Panggil file PNG kamu
+      manifest: {
+        name: "Al-Qur'an Indonesia Modern",
+        short_name: 'Al-Quran',
+        description: 'Aplikasi Al-Quran Digital Terlengkap',
+        theme_color: '#020617',
+        background_color: '#020617',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'OIP.png', 
+            sizes: '192x192', 
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'OIP.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      }
+    })
+  ]
 })
